048bb39aa811555e576cb89f44d2d44b777cfb73
Camilo del Real <cjdelreal2009@gmail.com>
2020-10-27 15:20:59 +0100

    EDIT: Increasing app version
    1.0.0-alpha

--- weather-x/buildSrc/src/main/kotlin/Dependencies.kt
+++ weather-x/buildSrc/src/main/kotlin/Dependencies.kt
@@ -21,10 +21,10 @@ object Build {
     const val BUILD_TOOLS_VERSION = "30.0.2"
     const val APPLICATION_ID = "org.xapps.apps.weatherx"
     const val APP_NAME = "WeatherX"
-    const val MAJOR_VERSION = 0
-    const val MINOR_VERSION = 9
+    const val MAJOR_VERSION = 1
+    const val MINOR_VERSION = 0
     const val PATH_VERSION = 0
-    const val STATUS_VERSION = ""
+    const val STATUS_VERSION = "-alpha"
 }
 
 object Repositories {
