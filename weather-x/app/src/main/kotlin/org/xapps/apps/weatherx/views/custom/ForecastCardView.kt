81aed4ab97448f467cdf92f0802443dc40a64443
Camilo del Real <cjdelreal2009@gmail.com>
2020-10-28 19:32:08 +0100

    EDIT: ForeCastView uses now data binding

--- weather-x/app/src/main/kotlin/org/xapps/apps/weatherx/views/custom/ForecastCardView.kt
+++ weather-x/app/src/main/kotlin/org/xapps/apps/weatherx/views/custom/ForecastCardView.kt
@@ -5,27 +5,29 @@ import android.content.res.ColorStateList
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.util.TypedValue
-import android.view.View
-import android.view.ViewTreeObserver
-import android.widget.FrameLayout
+import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.widget.ImageViewCompat
 import androidx.databinding.BindingAdapter
 import kotlinx.android.synthetic.main.content_card_forecast.view.*
 import org.xapps.apps.weatherx.R
+import org.xapps.apps.weatherx.databinding.ContentCardForecastBinding
 import org.xapps.apps.weatherx.services.models.Current
 import timber.log.Timber
 import java.util.*
-import kotlin.math.min
 
 
-class ForecastCardView(context: Context, attrs: AttributeSet) :
-    FrameLayout(context, attrs), ViewTreeObserver.OnPreDrawListener {
-    private var mStickyWidth = STICKY_WIDTH_UNDEFINED
+class ForecastCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0,
+    defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyle, defStyleRes) {
 
     private var clickableButton = false
 
-    private fun init(context: Context, attrs: AttributeSet) {
+    init {
         val a: TypedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ForecastCardView)
         val icon = a.getResourceId(R.styleable.ForecastCardView_fc_icon, -1)
         val iconTint = a.getColor(R.styleable.ForecastCardView_fc_iconTint, -1)
@@ -35,7 +37,8 @@ class ForecastCardView(context: Context, attrs: AttributeSet) :
         clickableButton = a.getBoolean(R.styleable.ForecastCardView_fc_clickable, false)
         a.recycle()
 
-        inflate(context, R.layout.content_card_forecast, this)
+        val layoutInflater = LayoutInflater.from(context)
+        ContentCardForecastBinding.inflate(layoutInflater, this, true)
 
         if(icon != -1) {
             imgIcon.setImageResource(icon)
@@ -76,85 +79,7 @@ class ForecastCardView(context: Context, attrs: AttributeSet) :
         }
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val desiredHeight = 10000
-        val desiredWidth: Int
-        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
-        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
-        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
-        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
-        val width: Int
-        val height: Int
-        height = when (heightMode) {
-            MeasureSpec.EXACTLY -> {
-                heightSize
-            }
-            MeasureSpec.AT_MOST -> {
-                min(desiredHeight, heightSize)
-            }
-            else -> {
-                desiredHeight
-            }
-        }
-        desiredWidth = when {
-            mStickyWidth != STICKY_WIDTH_UNDEFINED -> {
-                mStickyWidth
-            }
-            height > BREAK_HEIGHT -> {
-                ARBITRARY_WIDTH_LESSER
-            }
-            else -> {
-                ARBITRARY_WIDTH_GREATER
-            }
-        }
-        width = when (widthMode) {
-            MeasureSpec.EXACTLY -> {
-                widthSize
-            }
-            MeasureSpec.AT_MOST -> {
-                min(desiredWidth, widthSize)
-            }
-            else -> {
-                desiredWidth
-            }
-        }
-        setMeasuredDimension(width, height)
-        val childCount = childCount
-        for (i in 0 until childCount) {
-            val v: View = getChildAt(i)
-            v.measure(widthMeasureSpec, heightMeasureSpec)
-        }
-    }
-
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        val w = right - left
-        val h = bottom - top
-        super.onLayout(changed, left, top, right, bottom)
-        if (h <= BREAK_HEIGHT && w < ARBITRARY_WIDTH_GREATER
-            && mStickyWidth == STICKY_WIDTH_UNDEFINED
-        ) {
-            mStickyWidth = ARBITRARY_WIDTH_GREATER
-            viewTreeObserver.addOnPreDrawListener(this)
-        } else {
-            mStickyWidth = STICKY_WIDTH_UNDEFINED
-        }
-    }
-
-    override fun onPreDraw(): Boolean {
-        viewTreeObserver.removeOnPreDrawListener(this)
-        if (mStickyWidth == STICKY_WIDTH_UNDEFINED) {
-            return true
-        }
-        requestLayout()
-        return false
-    }
-
     companion object {
-        private const val STICKY_WIDTH_UNDEFINED = -1
-        private const val BREAK_HEIGHT = 1950
-        private const val ARBITRARY_WIDTH_LESSER = 200
-        private const val ARBITRARY_WIDTH_GREATER = 800
-
 
         @JvmStatic
         @BindingAdapter("customValue")
@@ -283,7 +208,4 @@ class ForecastCardView(context: Context, attrs: AttributeSet) :
 
     }
 
-    init {
-        init(context, attrs)
-    }
 }
