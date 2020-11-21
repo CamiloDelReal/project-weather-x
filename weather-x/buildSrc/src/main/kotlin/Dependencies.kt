661019de710252908bd37b9ae72aae48f6d63a00
Camilo del Real <cjdelreal2009@gmail.com>
2020-11-21 15:27:26 +0100

    EDIT: Version 1.1.0 final release

--- weather-x/buildSrc/src/main/kotlin/Dependencies.kt
+++ weather-x/buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@ object Build {
     const val MAJOR_VERSION = 1
     const val MINOR_VERSION = 1
     const val PATH_VERSION = 0
-    const val STATUS_VERSION = "-snapshot"
+    const val STATUS_VERSION = ""
 }
 
 object Repositories {
