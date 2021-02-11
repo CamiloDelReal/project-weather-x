55409a2dd2a883cde0427b295393b1d72f8ae462
Camilo del Real <cjdelreal2009@gmail.com>
2021-02-11 12:28:26 +0100

    FIX: Bad sizes for low resolution devices

---
+++ weather-x/app/src/main/kotlin/org/xapps/apps/weatherx/views/bindings/LinearLayoutBindings.kt
@@ -0,0 +1,26 @@
+package org.xapps.apps.weatherx.views.bindings
+
+import android.view.View
+import android.widget.LinearLayout
+import androidx.databinding.BindingAdapter
+import org.xapps.apps.weatherx.services.models.Daily
+
+
+object LinearLayoutBindings {
+
+    @JvmStatic
+    @BindingAdapter("visibility")
+    fun visibility(layout: LinearLayout, value: Daily?) {
+        val visibilityValue = value?.let { info ->
+            if (info.visibility == -1) {
+                View.GONE
+            }  else {
+                View.VISIBLE
+            }
+        } ?: run {
+            View.GONE
+        }
+        layout.visibility = visibilityValue
+    }
+
+}
