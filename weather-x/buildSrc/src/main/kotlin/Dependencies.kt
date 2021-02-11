920caacb0fbc4e2544f881b7e96b17436d9fc89a
Camilo del Real <cjdelreal2009@gmail.com>
2021-02-11 12:29:10 +0100

    EDIT: Version 1.2.6 final release

--- weather-x/buildSrc/src/main/kotlin/Dependencies.kt
+++ weather-x/buildSrc/src/main/kotlin/Dependencies.kt
@@ -23,7 +23,7 @@ object Build {
     const val APP_NAME = "WeatherX"
     const val MAJOR_VERSION = 1
     const val MINOR_VERSION = 2
-    const val PATH_VERSION = 5
+    const val PATH_VERSION = 6
     const val STATUS_VERSION = ""
 }
 
